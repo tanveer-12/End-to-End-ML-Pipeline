777ec62df8fb23d4b275182e95fb52ddd86d69b5
Tanveer kaur <tanveerkaur1292@outlook.com>
2025-11-03 11:13:34 -0500

    data preprocessing component finished

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -57,4 +57,61 @@ def preprocess_df(df, text_column='text', target_column='target'):
     """
     Preprocesses the DataFrame by encoding the target column, removing duplicates, and transforming the text column.
     """
+    try:
+        logger.debug('Starting preprocessing for DataFrame')
+        # Encode the target column
+        encoder = LabelEncoder()
+        df[target_column] = encoder.fit_transform(df[target_column])
+        logger.debug('Target column encoded')
+
+        # remove duplicated rows
+        df = df.drop_duplicates(keep='first')
+        logger.debug('Duplicates removed')
+
+        # Apply text transformations to the specified text column
+        df.loc[:, text_column] = df[text_column].apply(transform_text)
+        logger.debug('Text column transformed')
+        return df
     
+    except KeyError as e:
+        logger.error('Column not found: %s', e)
+        raise
+
+    except Exception as e:
+        logger.error('Error during text normalization: %s', e)
+        raise
+
+
+def main(text_column='text', target_column='target'):
+    """
+    Main function to load raw data, preprocess it, and save the processed data
+    """
+    try:
+        # Fetch the data from data/raw
+        train_data = pd.read_csv('./data/raw/train.csv')
+        test_data = pd.read_csv('./data/raw/test.csv')
+        logger.debug('Data loaded properly')
+
+        # Transform the data
+        train_preprocessed_data = preprocess_df(train_data, text_column, target_column)
+        test_preprocessed_data = preprocess_df(test_data, text_column, target_column)
+
+        # Store the data inside data/preprocessed
+        data_path = os.path.join("./data","interim")
+        os.makedirs(data_path, exist_ok=True)
+
+        train_preprocessed_data.to_csv(os.path.join(data_path, "train_processed.csv"), index=False)
+        test_preprocessed_data.to_csv(os.path.join(data_path, "test_preprocessed.csv"), index=False)
+
+        logger.debug('Processed data saved to %s', data_path)
+    except FileNotFoundError as e:
+        logger.error('File not found: %s', e)
+    except pd.errors.EmptyDataError as e:
+        logger.error('No data: %s', e)
+    except Exception as e:
+        logger.error('Failed to complete the data transformation process: %s', e)
+        print(f"Error: {e}")
+
+
+if __name__ == '__main__':
+    main()
