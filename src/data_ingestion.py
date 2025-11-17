cc5662cbe5f33ab2f0a49bc9c4b8f204de817b18
Tanveer kaur <tanveerkaur1292@outlook.com>
2025-11-17 09:33:58 -0500

    using yaml

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -27,6 +27,23 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
+
 
 def load_data(data_url: str) -> pd.DataFrame:
     """Load dara from a CSV File"""
@@ -42,7 +59,7 @@ def load_data(data_url: str) -> pd.DataFrame:
         raise
 
 
-def preprocess_date(df: pd.DataFrame) -> pd.DataFrame:
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     """Preprocess the data"""
     try:
         df.drop(columns = ['Unnamed: 2','Unnamed: 3', 'Unnamed: 4'], inplace=True)
@@ -61,9 +78,11 @@ def save_data(train_date: pd.DataFrame, test_date: pd.DataFrame, data_path: str)
     """Save the train and test datasets"""
     try:
         raw_data_path = os.path.join(data_path, 'raw')
+        raw_data_path = os.path.normpath(raw_data_path)  # normalize path for Windows/Linux
         os.makedirs(raw_data_path, exist_ok=True)
         train_date.to_csv(os.path.join(raw_data_path, "train.csv"), index=False)
         test_date.to_csv(os.path.join(raw_data_path, "test.csv"), index=False)
+        #log_path = raw_data_path.replace("\\", "/")
         logger.debug('Train and test data saved to %s', raw_data_path)
     except Exception as e:
         logger.error('Unexpected error occured while saving the data: %s', e)
@@ -72,10 +91,12 @@ def save_data(train_date: pd.DataFrame, test_date: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        # test_size = 0.2
         data_path = "https://raw.githubusercontent.com/tanveer-12/End-to-End-ML-Pipeline/refs/heads/main/experiments/spam.csv"
         df = load_data(data_url=data_path)
-        final_df = preprocess_date(df)
+        final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
         save_data(train_data, test_data, data_path='./data')
     except Exception as e:
