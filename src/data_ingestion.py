e3a0967186ebac33de1f9ca446bfce1dc2cdfd4e
Tanveer kaur <tanveerkaur1292@outlook.com>
2025-12-16 15:42:36 -0500

    data ingestion and preprocessing working fine

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,48 +4,32 @@ from sklearn.model_selection import train_test_split
 import logging
 import yaml
 
-logger = logging.getLogger("data_ingestion")
-logger.setLevel(logging.DEBUG)
+# Ensure the "logs" directory exists
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
 
-if not logger.handlers:   
-    log_dir = "logs"
-    os.makedirs(log_dir, exist_ok=True)
+# logging configuration
+logger = logging.getLogger('data_ingestion')
+logger.setLevel('DEBUG')
 
-    log_file_path = os.path.join(log_dir, "data_ingestion.log")
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
 
-    console_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler(log_file_path, mode="a", encoding="utf-8")
+log_file_path = os.path.join(log_dir, 'data_ingestion.log')
 
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
 
-    console_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
 
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
-
-# def load_params(params_path: str) -> dict:
-#     """Load parameters from a YAML file."""
-#     try:
-#         with open(params_path, 'r') as file:
-#             params = yaml.safe_load(file)
-#         logger.debug('Parameters retrieved from %s', params_path)
-#         return params
-#     except FileNotFoundError:
-#         logger.error('File not found: %s', params_path)
-#         raise
-#     except yaml.YAMLError as e:
-#         logger.error('YAML error: %s', e)
-#         raise
-#     except Exception as e:
-#         logger.error('Unexpected error: %s', e)
-#         raise
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 
 def load_data(data_url: str) -> pd.DataFrame:
-    """Load data from a CSV File"""
+    """Load dara from a CSV File"""
     try:
         df = pd.read_csv(data_url)
         logger.debug('Data loaded from %s', data_url)
@@ -58,7 +42,7 @@ def load_data(data_url: str) -> pd.DataFrame:
         raise
 
 
-def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+def preprocess_date(df: pd.DataFrame) -> pd.DataFrame:
     """Preprocess the data"""
     try:
         df.drop(columns = ['Unnamed: 2','Unnamed: 3', 'Unnamed: 4'], inplace=True)
@@ -76,15 +60,10 @@ def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
 def save_data(train_date: pd.DataFrame, test_date: pd.DataFrame, data_path: str) -> None:
     """Save the train and test datasets"""
     try:
-        # raw_data_path = os.path.join(data_path, 'raw')
-        # raw_data_path = os.path.normpath(raw_data_path)  # normalize path for Windows/Linux
-        
-        # Convert to absolute path to avoid relative path issues
-        raw_data_path = os.path.abspath(os.path.join(data_path, 'raw'))
+        raw_data_path = os.path.join(data_path, 'raw')
         os.makedirs(raw_data_path, exist_ok=True)
         train_date.to_csv(os.path.join(raw_data_path, "train.csv"), index=False)
         test_date.to_csv(os.path.join(raw_data_path, "test.csv"), index=False)
-        #log_path = raw_data_path.replace("\\", "/")
         logger.debug('Train and test data saved to %s', raw_data_path)
     except Exception as e:
         logger.error('Unexpected error occured while saving the data: %s', e)
@@ -93,26 +72,16 @@ def save_data(train_date: pd.DataFrame, test_date: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        # params = load_params(params_path='params.yaml')
-        # test_size = params['data_ingestion']['test_size']
         test_size = 0.2
-        DATA_URL = "https://raw.githubusercontent.com/tanveer-12/End-to-End-ML-Pipeline/refs/heads/main/experiments/spam.csv"
-        RAW_DATA_DIR = "data"
-        df = load_data(data_url=DATA_URL)
-        final_df = preprocess_data(df)
+        data_path = "https://raw.githubusercontent.com/tanveer-12/End-to-End-ML-Pipeline/refs/heads/main/experiments/spam.csv"
+        df = load_data(data_url=data_path)
+        final_df = preprocess_date(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
-        save_data(train_data, test_data, data_path=RAW_DATA_DIR)
+        save_data(train_data, test_data, data_path='./data')
     except Exception as e:
         logger.error('Failed to complete the data ingestion process: %s', e)
         print(f"Error: {e}")
-    finally:
-        # RELEASE FILE HANDLES (Windows + DVC fix)
-        handlers = logger.handlers[:]
-        for handler in handlers:
-            handler.close()
-            logger.removeHandler(handler)
 
 
 if __name__ == '__main__':
     main()
-        
