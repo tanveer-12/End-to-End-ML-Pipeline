803915e09a70df08bdf184df627a25b198801814
Tanveer kaur <tanveerkaur1292@outlook.com>
2025-12-16 16:40:01 -0500

    each file works up to this commit

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -6,7 +6,6 @@ import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 import logging
 import yaml
-from dvclive import Live
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -119,15 +118,7 @@ def main():
         X_test = test_data.iloc[:, :-1].values
         y_test = test_data.iloc[:, -1].values
 
-        metrics = evaluate_model(clf, X_test, y_test)
-
-        # Experiment tracking using dvclive
-        # with Live(save_dvc_exp=True) as live:
-        #     live.log_metric('accuracy', accuracy_score(y_test, y_test))
-        #     live.log_metric('precision', precision_score(y_test, y_test))
-        #     live.log_metric('recall', recall_score(y_test, y_test))
-        #     live.log_params(params)
-        
+        metrics = evaluate_model(clf, X_test, y_test)        
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
         logger.error('Failed to complete the model evaluation process: %s', e)
